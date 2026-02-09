fa78ecd011e958ee52a662063c45f07c967ec15b
Web_senai <Rafael.a.silva65@aluno.senai.br>
2026-02-09 16:34:22 -0300

    init

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
 
 @app.route('/')
 def home():
@@ -19,6 +20,14 @@ def geometria():
 def operacoes():
     return render_template("operacoes.html")
 
+@app.route('/funcionarios')
+def funcionarios():
+    return render_template("funcionarios.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
 @app.route('/somar', methods=['GET', 'POST'])
 def somar():
     if request.method == 'POST':
@@ -26,7 +35,11 @@ def somar():
             n1 = int(request.form['form-n1'])
             n2 = int(request.form['form-n2'])
             soma = n1 + n2
+            flash('Soma realizada', 'alert-success')
             return render_template("operacoes.html", n1=n1, n2=n2, soma=soma)
+        else:
+            # Passo 1: Emitir a mensagem e a categoria do flash
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("operacoes.html")
 
@@ -37,7 +50,11 @@ def subtrair():
             n1 = int(request.form['form-n1'])
             n2 = int(request.form['form-n2'])
             sub = n1 - n2
+            flash('Soma realizada', 'alert-success')
             return render_template("operacoes.html", n1=n1, n2=n2, sub=sub)
+        else:
+            flash('Preencha o campo', 'alert-danger')
+
 
     return render_template("operacoes.html")
 
@@ -48,7 +65,11 @@ def multiplicar():
             n1 = int(request.form['form-n1'])
             n2 = int(request.form['form-n2'])
             multi = n1 * n2
+            flash('Soma realizada', 'alert-success')
             return render_template("operacoes.html", n1=n1, n2=n2, multi=multi)
+        else:
+            flash('Preencha o campo', 'alert-danger')
+
 
     return render_template("operacoes.html")
 
@@ -59,7 +80,11 @@ def dividir():
             n1 = int(request.form['form-n1'])
             n2 = int(request.form['form-n2'])
             divi = n1 / n2
+            flash('Soma realizada', 'alert-success')
             return render_template("operacoes.html", n1=n1, n2=n2, divi=divi)
+        else:
+            flash('Preencha o campo', 'alert-danger')
+
 
     return render_template("operacoes.html")
 
@@ -70,7 +95,10 @@ def triangulo():
             n1 = float(request.form['form-n1'])
             n2 = float(request.form['form-n2'])
             triangulo = n1 * n2 / 2
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, n2=n2, triangulo=triangulo)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
 
@@ -82,7 +110,10 @@ def perimetro_tria():
             n2 = float(request.form['form-n2'])
             n3 = float(request.form['form-n3'])
             perimetro_tria = n1 + n2 + n3
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, n2=n2, n3=n3, perimetro_tria=perimetro_tria)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
 
@@ -93,7 +124,10 @@ def area_circ():
             n1 = float(request.form['form-n1'])
             n2 = float(request.form['form-n2'])
             area_circ = n1 * n2 * 3.14
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, n2=n2, area_circ=area_circ)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
 
@@ -103,7 +137,10 @@ def perimetro_circ():
         if request.form['form-n1']:
             n1 = float(request.form['form-n1'])
             perimetro_circ = n1 * 3.14
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, perimetro_circ=perimetro_circ)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
 
@@ -114,7 +151,10 @@ def area_quad():
             n1 = float(request.form['form-n1'])
             n2 = float(request.form['form-n2'])
             area_quad = n1 * n2
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, n2=n2, area_quad=area_quad)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
 
@@ -127,9 +167,43 @@ def perimetro_quad():
             n3 = float(request.form['form-n3'])
             n4 = float(request.form['form-n4'])
             perimetro_quad = n1 + n2 + n3 + n4
+            flash('Soma realizada', 'alert-success')
             return render_template("geometria.html", n1=n1, n2=n2, n3=n3, n4=n4, perimetro_quad=perimetro_quad)
+        else:
+            flash('Preencha o campo', 'alert-danger')
 
     return render_template("geometria.html")
+
+@app.route('/area_hexa', methods=['GET', 'POST'])
+def area_hexa():
+    if request.method == 'POST':
+        if request.form['form_area_hexa']:
+            lado_h = float(request.form['form_area_hexa'])
+            area_h = 3 * pow(lado_h, 2) * 3 ** 0.5 / 2
+            flash('Soma realizada', 'alert-success')
+            return render_template("geometria.html", area_hexa=area_hexa, area_h=round (area_h, 2), lado_h=lado_h)
+        else:
+            flash('Preencha o campo', 'alert-danger')
+
+    return render_template("geometria.html")
+
+@app.route('/perimetro_hexa', methods=['GET', 'POST'])
+def perimetro_hexa():
+    if request.method == 'POST':
+        if request.form['form-n1'] and request.form['form-n2'] and request.form['form-n3'] and request.form['form-n4'] and request.form['form-n5'] and request.form['form-n6']:
+            n1 = float(request.form['form-n1'])
+            n2 = float(request.form['form-n2'])
+            n3 = float(request.form['form-n3'])
+            n4 = float(request.form['form-n4'])
+            n5 = float(request.form['form-n5'])
+            n6 = float(request.form['form-n6'])
+            perimetro_hexa = n1 + n2 + n3 + n4 + n5 + n6
+            flash('Soma realizada', 'alert-success')
+            return render_template(geometria.html, n1=n1, n2=n2, n3=n3, n4=n4, n5=n5, n6=n6, perimetro_hexa=perimetro_hexa)
+        else:
+            flash('Preencha o campo', 'alert-danger')
+
+        return render_template("geometria.html")
 
 
     return render_template("geometria.html")
